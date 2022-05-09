d46935834840dbf6f161c43db94c05843b6d6e9b
morjep <94778792+morjep@users.noreply.github.com>
2022-05-09 20:20:57 +0200

    Added routing of search and root

---
+++ starter/src/Search.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Link } from "react-router-dom";
+
+const Search = () => {
+  return (
+    <div className="app">
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">
+            Close
+          </Link>
+
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title, author, or ISBN" />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
